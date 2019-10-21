db71f3f36a1e202c4829ce9a70962e3f84d23a46
Yining Wang <yining.wang@utexas.edu>
2019-10-21 23:12:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.includes?
+  calls.include?{|call| call.length >= 4}
 end
 
 def find_the_cheese# code an argument here
