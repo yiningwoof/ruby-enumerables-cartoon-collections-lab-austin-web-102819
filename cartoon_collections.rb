24cb91f35a10930ec4c030116aa092beb7294d07
Yining Wang <yining.wang@utexas.edu>
2019-10-21 23:03:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  new = planeteer_calls.map(|call| "#{call.capitalize}!)
-  new
+  new_calls = planeteer_calls.map(|call| "#{call.capitalize}!)
+  new_calls
 end
 
 def long_planeteer_calls# code an argument here
