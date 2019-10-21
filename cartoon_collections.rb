0cced9d2043186cfce90ea32fad5029e0e7011fe
Yining Wang <yining.wang@utexas.edu>
2019-10-21 23:20:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(calls)
   calls.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strs)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strs.find_index()
 end
